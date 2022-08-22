7336dab1d5c69361e4d0b8975b9a94e8e87ee489
gabrieldasilvadev <gabriel.dsilva@sptech.school>
2022-08-21 22:32:08 -0300

    atividade lista2 v3

--- lista02/src/main/java/sptech/school/atividades/lista02/Potencia.java
+++ lista02/src/main/java/sptech/school/atividades/lista02/Potencia.java
@@ -11,10 +11,23 @@ import java.util.Scanner;
  * @author gabriel
  */
 public class Potencia {
+
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    
-    
+
+    System.out.println("Informe a base: ");
+    Integer base = scan.nextInt();
+
+    System.out.println("Informe o expoente: ");
+    Integer expoente = scan.nextInt();
+
+    Integer resultado = 1;
+
+    for (int i = expoente; i >= 1; i--) {
+      resultado *= base;
+    }
+
+    System.out.println("Resultado: " + resultado);
   }
-  
+
 }
